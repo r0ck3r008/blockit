f4cd3f170869edd8fe9a7f92753acf51c6fc5666
r0ck3r008 <naman.arora@tutanota.com>
2019-05-21 23:49:06 +0530

    Unix domain server subroutines

---
+++ src/server/uds_workings.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<errno.h>
+
+#include"uds_workings.h"
+#include"utils.h"
+
+int sock_create(char *addr)
+{
+	int sock=-1;
+	if((sock=socket(AF_UNIX, SOCK_STREAM, 0))<0){
+		fprintf(stderr, "[-]Error in creating UDS socket: %s\n",
+			strerror(errno));
+		goto exit;
+	}
+
+	if(unlink(addr)<0 && errno!=2){
+		fprintf(stderr, "[-]Error in unlinking the address: %s\n",
+			strerror(errno));
+		goto exit;
+	}
+
+	struct sockaddr_un addr_un;
+	explicit_bzero(&addr, SUN_LEN(&addr_un));
+	addr_un.sun_family=AF_UNIX;
+	strcpy(addr_un.sun_path, addr);
+
+	if(bind(sock, (struct sockaddr *)&addr_un, SUN_LEN(&addr_un))<0){
+		fprintf(stderr, "[-]Error in binding the UDS server sock: %s\n",
+			strerror(errno));
+	}
+
+exit:
+	return sock;
+}
+
+void *uds_workings(void *a)
+{
+	if(a==NULL){
+		fprintf(stderr, "[-]Error in receving the UDS name\n");
+		pthread_exit(NULL);
+	}
+
+	char *addr=a;
+	int uds_sock=sock_create(addr);
+
+	//server loop
+	pthread_t tid[10];
+	for(int i=0; ;){
+		int sock=0, stat;
+		if((sock=accept(uds_sock, NULL, NULL))<0){
+			fprintf(stderr, "[-]Error in accepting client %d: %s\n",
+				i, strerror(errno));
+			continue;
+		}
+
+		if((stat=pthread_create(&tid[i], NULL, uds_cli_run, &sock))!=0){
+			fprintf(stderr, "[-]Error in starting client %d: %s\n",
+				i, strerror(errno));
+			continue;
+		}
+
+		i++;
+	}
+}
+
+void *uds_cli_run(void *a)
+{
+	int sock=*(int *)a;
+
+	//work_loop
+}
---
+++ src/server/uds_workings.h
@@ -0,0 +1,8 @@
+#ifndef UDS_WORKINGS_H
+#define UDS_WORKINGS_H
+
+int sock_create(char *);
+void *uds_workings(void *);
+void *uds_cli_run(void *);
+
+#endif
