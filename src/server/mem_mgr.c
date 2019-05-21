8cb619850c07745f0481e780565a6d5ca17e9a84
r0ck3r008 <naman.arora@tutanota.com>
2019-05-21 23:47:58 +0530

    source for the allocate and the deallocate subroutines

---
+++ src/server/mem_mgr.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+#include"mem_mgr.h"
+
+void *alloc(char *type, int size)
+{
+	void *ret=NULL;
+
+	if(!strcmp(type, "char")){
+		ret=malloc(sizeof(char)*size);
+		explicit_bzero(ret, sizeof(char)*size);
+	}
+
+	if(ret==NULL){
+		fprintf(stderr, "[-]Error in allocating %d bytes for %s type\n",
+			size, type);
+		_exit(-1);
+	}
+
+	return ret;
+}
+
+void dealloc(char *type, int size, void *buf)
+{
+	if(!strcmp(type, "char")){
+		explicit_bzero(buf, sizeof(char)*size);
+	}
+
+	free(buf);
+}
