bf01ebcc8f9ccd41f6d4e20764de667754a9e941
r0ck3r008 <naman.arora@tutanota.com>
2019-05-21 23:48:42 +0530

    misc utility subroutines needed by many other modules

---
+++ src/server/utils.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<errno.h>
+
+#include"utils.h"
+#include"mem_mgr.h"
+
+int init_args(struct arg *arguments, int argc, char **argv)
+{
+	add_argument(arguments, "-u", "--uds_path",
+		"The UDS path for server", 1);
+	add_argument(arguments, "-m", "--mirrorlist",
+		"The mirrorlist to fetch blocklists from", 1);
+
+	if(argc!=3){
+		show_help(arguments);
+		clean(arguments);
+		return 1;
+	}
+
+	parse_args(arguments, argc, argv);
+	return 0;
+}
+
+char *find_arg(struct arg *arguments, char *s_name)
+{
+	char *ret=NULL;
+	struct arg *curr=arguments;
+	for(curr; curr->nxt!=NULL || !strcmp(curr->s_name, s_name);
+		curr=curr->nxt){
+		ret=curr->value;
+	}
+
+	return ret;
+}
+
+void wrt(int sock, char *cmds, char *reason)
+{
+	if(write(sock, cmds, 512)<0){
+		fprintf(stderr, "[-]Error in writing %s for %s: %s\n",
+			cmds, reason, strerror(errno));
+	}
+
+	dealloc("char", 512, cmds);
+}
+
+char *rd(int sock, char *reason)
+{
+	char *cmdr=alloc("char", 512);
+
+	if(read(sock, cmdr, 512)<0){
+		fprintf(stderr, "[-]Error in reading for %s: %s\n",
+			reason, strerror(errno));
+		dealloc("char", 512, cmdr);
+		cmdr=NULL;
+	}
+
+	return cmdr;
+}
---
+++ src/server/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifdef NEED_ARGS
+	#include"cargparse/cargparse.h"
+
+	int init_args(struct arg *, int, char **);
+	char *find_arg(struct arg *, char *);
+#endif
+
+void wrt(int, char *, char *);
+char *rd(int, char *);
+
+#endif
