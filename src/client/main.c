d34b2f2be5ff65ef53eb3d4bb33950b17e0fe33a
r0ck3r008 <naman.arora@tutanota.com>
2019-06-04 00:07:04 +0530

    added a previously ommited function call to uds_workings

--- src/client/main.c
+++ src/client/main.c
@@ -3,10 +3,12 @@
 #include<stdio.h>
 
 #include"utils.h"
+#include"uds_workings.h"
 
 int main(int argc, char * argv[])
 {
 	struct arg *arguments=init_lib();
 	init_args(arguments, argc, argv);
 
+	uds_workings(find_arg_val(arguments, "uds_sock"));
 }
