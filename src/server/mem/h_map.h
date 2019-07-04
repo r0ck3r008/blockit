c1fc0c95b050cabe4a98e55bde9aa1e243f6d2a2
r0ck3r008 <naman.arora@tutanota.com>
2019-07-05 00:58:48 +0530

    denition of struct node is now in h_map.h

--- src/server/mem/h_map.h
+++ src/server/mem/h_map.h
@@ -1,6 +1,13 @@
 #ifndef H_MAP_H
 #define H_MAP_H
 
+struct node
+{
+	char *ip;
+	struct node *nxt;
+	struct node *prev;
+};
+
 struct node **h_map_create(int);
 void h_map_insert(struct node **, char *, int);
 int h_map_find(struct node **, char *, int);
