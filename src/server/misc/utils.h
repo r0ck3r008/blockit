c482464bd8fab7fc83aa79cb99e4310540b6994c
r0ck3r008 <naman.arora@tutanota.com>
2019-07-09 03:32:02 +0530

    added a function to fetch url from the mirror list, also a moved openf subroutine from transfer_utils to here to provide more generalized file opener

--- src/server/misc/utils.h
+++ src/server/misc/utils.h
@@ -5,9 +5,12 @@
 	#include"cargparse/cargparse.h"
 
 	void init_args(struct arg *, int, char **);
+	struct arg *arguments_glbl;
 #endif
 
 void wrt(int, char *, char *);
 int rd(int, char *, char *);
+FILE *openf(char *, int);
+char *fetch_url();
 
 #endif
