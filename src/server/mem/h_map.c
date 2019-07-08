7bca8363a5140f5bdca8eed0ddda38ffc5566692
r0ck3r008 <naman.arora@tutanota.com>
2019-07-08 20:14:36 +0530

    added a new datatype struct, h_map_t to abstract the node ** h_map and int rows attributes of an hash map definition, also made h_map module compatible with new node module

--- src/server/mem/h_map.c
+++ src/server/mem/h_map.c
@@ -1,34 +1,34 @@
-#define NEEDS_LIST
-
-#include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
 
 #include"mem_mgr.h"
 #include"h_map.h"
 
-struct node **h_map_create(int rows)
+struct h_map_t *h_map_create(int rows)
 {
+	struct h_map_t *h_map_st=alloc("struct h_map_t", 1);
 	struct node **h_map=alloc("struct node *", rows);
 	for(int i=0; i<rows; i++){
 		h_map[i]=alloc_node("start");
 	}
 
-	return h_map;
+	h_map_st->h_map=h_map;
+	h_map_st->rows=rows;
+
+	return h_map_st;
 }
 
-void h_map_insert(struct node **h_map, char *ip, int rows)
+void h_map_insert(struct h_map_t *h_map, char *ip)
 {
-	int key=h_map_key(ip, rows);
+	int key=h_map_key(ip, h_map->rows);
 	struct node *new=alloc_node(ip);
-	add_node(h_map[key], new);
+	add_node(h_map->h_map[key], new);
 }
 
-int h_map_find(struct node **h_map, char *ip, int rows)
+int h_map_find(struct h_map_t *h_map, char *ip)
 {
 	int ret=0;
-	int key=h_map_key(ip, rows);
-	struct node *curr=find_memb(h_map[key], ip);
+	int key=h_map_key(ip, h_map->rows);
+	struct node *curr=find_memb(h_map->h_map[key], ip);
 	if(curr!=NULL){
 		ret=1;
 	}
@@ -49,11 +49,11 @@ int h_map_key(char *ip, int rows)
 	return count%rows;
 }
 
-void h_map_clean(struct node **h_map, int rows)
+void h_map_clean(struct h_map_t *h_map)
 {
-	for(int i=0; i<rows; i++){
-		del_list(h_map[i]);
+	for(int i=0; i<h_map->rows; i++){
+		del_list(h_map->h_map[i]);
 	}
 
-	dealloc("struct node *", rows, h_map);
+	dealloc("struct node *", h_map->rows, h_map->h_map);
 }
--- src/server/mem/h_map.h
+++ src/server/mem/h_map.h
@@ -1,17 +1,17 @@
 #ifndef H_MAP_H
 #define H_MAP_H
 
-struct node
-{
-	char *ip;
-	struct node *nxt;
-	struct node *prev;
+#include"node.h"
+
+struct h_map_t{
+	struct node **h_map;
+	int rows;
 };
 
-struct node **h_map_create(int);
-void h_map_insert(struct node **, char *, int);
-int h_map_find(struct node **, char *, int);
+struct h_map_t *h_map_create(int);
+void h_map_insert(struct h_map_t *, char *);
+int h_map_find(struct h_map_t *, char *);
 int h_map_key(char *, int);
-void h_map_clean(struct node **, int);
+void h_map_clean(struct h_map_t *);
 
 #endif
