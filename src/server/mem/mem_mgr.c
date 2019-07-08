de16557600c5a683bda47bafaa31597bd4d28328
r0ck3r008 <naman.arora@tutanota.com>
2019-07-08 20:15:18 +0530

    saperated linked list and mem_mgr subroutines

--- src/server/mem/mem_mgr.c
+++ src/server/mem/mem_mgr.c
@@ -1,5 +1,3 @@
-#define NEEDS_LIST
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -21,6 +19,9 @@ void *alloc(char *type, int size)
 	} else if(!strcmp(type, "struct node *")){
 		ret=malloc(sizeof(struct node *)*size);
 		explicit_bzero(ret, sizeof(struct node *)*size);
+	} else if(!strcmp(type, "struct h_map_t")){
+		ret=malloc(sizeof(struct h_map_t)*size);
+		explicit_bzero(ret, sizeof(struct h_map_t)*size);
 	}
 
 	if(ret==NULL){
@@ -40,67 +41,9 @@ void dealloc(char *type, int size, void *buf)
 		explicit_bzero(buf, sizeof(struct node)*size);
 	} else if(!strcmp(type, "struct node *")){
 		explicit_bzero(buf, sizeof(struct node *)*size);
+	} else if(!strcmp(type, "struct h_map_t")){
+		explicit_bzero(buf, sizeof(struct h_map_t)*size);
 	}
 
 	free(buf);
-}
-
-struct node *alloc_node(char *ip)
-{
-	struct node *curr=alloc("struct node", 1);
-	curr->nxt=NULL;
-	curr->prev=NULL;
-	curr->ip=alloc("char", 20);
-	sprintf(curr->ip, "%s", ip);
-	return curr;
-}
-
-void dealloc_node(struct node *curr)
-{
-	dealloc("char", 20, curr->ip);
-	dealloc("struct node", 1, curr);
-}
-
-void add_node(struct node *start, struct node *new)
-{
-	struct node *curr=start;
-	for(curr; curr->nxt!=NULL; curr=curr->nxt);
-
-	curr->nxt=new;
-	new->prev=curr;
-}
-
-void del_node(struct node *curr)
-{
-	curr->prev->nxt=curr->nxt;
-	if(curr->nxt!=NULL)
-		curr->nxt->prev=curr->prev;
-	dealloc_node(curr);
-}
-
-void del_list(struct node *start)
-{
-	struct node *curr=start;
-	for(curr; curr->nxt!=NULL; ){
-		del_node(curr->nxt);
-	}
-
-	dealloc_node(curr);
-}
-
-struct node *find_memb(struct node *start, char *ip)
-{
-	struct node *curr=start;
-	int flag=0;
-	for(curr; curr!=NULL; curr=curr->nxt){
-		if(!strcmp(curr->ip, ip)){
-			flag=1;
-			break;
-		}
-	}
-
-	if(flag){
-		return curr;
-	}
-	return NULL;
 }
--- src/server/mem/mem_mgr.h
+++ src/server/mem/mem_mgr.h
@@ -1,16 +1,6 @@
 #ifndef MEM_MGR_H
 #define MEM_MGR_H
 
-#ifdef NEEDS_LIST
-
-	struct node *alloc_node(char *);
-	void dealloc_node(struct node *);
-	void add_node(struct node *, struct node *);
-	void del_node(struct node *);
-	void del_list(struct node *);
-	struct node *find_memb(struct node *start, char *);
-#endif
-
 void *alloc(char *, int);
 void dealloc(char *, int, void *);
 
